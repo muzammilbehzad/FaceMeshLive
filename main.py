cb90dfb1969c5715621895252babba62cc8579ea
Muzammil Behzad <muzammil.behzad@gmail.com>
2021-04-07 14:45:26 +0300

    changes

--- main.py
+++ main.py
@@ -21,14 +21,34 @@ mp_face_mesh = mp.solutions.face_mesh
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
 # create the haar cascade for face detection
-faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+faceCascade = cv2.CascadeClassifier(r"C:\Users\mbehzad18\AppData\Local\Continuum\anaconda3\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml")
 
 # initialize camera capture
 video = cv2.VideoCapture(0)
 
 flip_image = True
+resize_image = True
 while True:
     ret, image = video.read()
+    if resize_image:
+        x_print, y_print, _ = image.shape
+        extend_size = 2.5
+        image = cv2.resize(image, (int(extend_size*y_print), int(extend_size*x_print)))
+        x_print, y_print, _ = image.shape
+        
+        x_print = int(0.15*x_print)
+        y_print = int(0.05*y_print)
+        text_size = 2
+        text_width = 6
+        color_to_print = (0,255,255)
+    else:
+        x_print, y_print, _ = image.shape
+        x_print = int(0.23*x_print)
+        y_print = int(0.05*y_print)
+        text_size = 0.7
+        text_width = 2
+        color_to_print = (0,0,255)
+
     if flip_image:
         image= cv2.flip(image, 1)
 
@@ -66,12 +86,9 @@ while True:
 
     now_time = datetime.datetime.now()
     window_title = 'Face Mesh Detection by Muzammil Behzad'
-    display_string = now_time.strftime("%A, %d %B, %Y | %H:%M:%S")
+    display_string = now_time.strftime("%A %d %B, %Y | %H:%M:%S")
 
-    x_print, y_print, _ = image.shape
-    x_print = int(0.23*x_print)
-    y_print = int(0.05*y_print)
-    cv2.putText(image, display_string, (x_print, y_print), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+    cv2.putText(image, display_string, (x_print, y_print), cv2.FONT_HERSHEY_SIMPLEX, text_size, color_to_print, text_width)
     cv2.imshow(window_title, image)
 
     k = cv2.waitKey(1)
